e9dd3443ea7e2db629903f169969b07d548819df
Christophe <da.costa.christophe.mail@gmail.com>
2022-07-19 13:53:58 +0200

    feat(domain): implement DeleteNote use case

---
+++ app/src/main/java/com/cdcoding/noteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,13 @@
+package com.cdcoding.noteapp.feature_note.domain.use_case
+
+import com.cdcoding.noteapp.feature_note.domain.model.Note
+import com.cdcoding.noteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
