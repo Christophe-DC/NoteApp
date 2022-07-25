d2ca9ea57fbc2dbf522a59ad66fbc4560ef2fd27
Christophe <da.costa.christophe.mail@gmail.com>
2022-07-25 14:40:55 +0200

    feat(AndroidTest) create NotesEndToEndTest UI tests

--- app/src/main/java/com/cdcoding/noteapp/feature_note/presentation/add_edit_note/components/AddEditNoteScreen.kt
+++ app/src/main/java/com/cdcoding/noteapp/feature_note/presentation/add_edit_note/components/AddEditNoteScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.cdcoding.noteapp.core.util.TestTags
 import com.cdcoding.noteapp.feature_note.domain.model.Note
 import com.cdcoding.noteapp.feature_note.presentation.add_edit_note.AddEditNoteEvent
 import com.cdcoding.noteapp.feature_note.presentation.add_edit_note.AddEditNoteViewModel
@@ -128,7 +129,8 @@ fun AddEditNoteScreen(
                 },
                 isHintVisible = titleState.isHintVisible,
                 singleLine = true,
-                textStyle = MaterialTheme.typography.h5
+                textStyle = MaterialTheme.typography.h5,
+                testTag = TestTags.TITLE_TEXT_FIELD
             )
             Spacer(modifier = Modifier.height(16.dp))
             TransparentHintTextField(
@@ -142,7 +144,8 @@ fun AddEditNoteScreen(
                 },
                 isHintVisible = contentState.isHintVisible,
                 textStyle = MaterialTheme.typography.body1,
-                modifier = Modifier.fillMaxHeight()
+                modifier = Modifier.fillMaxHeight(),
+                testTag = TestTags.CONTENT_TEXT_FIELD
             )
 
         }
